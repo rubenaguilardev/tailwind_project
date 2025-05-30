d09908e96eb38060502c08e63c43b4c4852bbd0b
rubenaguilardev <ragui56@wgu.edu>
2025-05-30 00:45:38 -0700

    complete header

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ export default function App() {
      <header>
       <section className='h-screen grid grid-cols-2 justify-items-center items-center'>
         <div className=''>
-          <img className='w-48' src={logo} alt="Future Fullstack logo" />
-          <h1>Become a fullstack developer with the world's best online bootcamp</h1>
-          <p>
+          <img className='w-48 mb-4' src={logo} alt="Future Fullstack logo" />
+          <h1 className='text-6xl font-bold text-slate-800 tracking-tight mb-4'>Become a fullstack developer with the world's best online bootcamp</h1>
+          <p className='mb-8'>
             Join the world's best and most affordable fullstack development bootcamp and master the skills to build 
             real-world applications with expert guidance, hands-on projects, and the latest industry tools.
           </p>
-          <ul>
-            <li><a href="#">Get started</a></li>
-            <li><a href="#">Learn more</a></li>
+          <ul className='flex gap-6'>
+            <li><a className='bg-sky-500 hover:bg-sky-700 text-white px-8 py-4 rounded-md inline-block' href="#">Get started</a></li>
+            <li><a className='bg-slate-400 hover:bg-slate-700 text-white px-8 py-4 rounded-md inline-block' href="#">Learn more</a></li>
           </ul>
         </div>
         <img className='w-3/4' src={dan} alt="Dan smiling while holding his laptop" />
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <body className='text-lg text-slate-600'>
     <App />
-  </StrictMode>,
+  </body>
 )
